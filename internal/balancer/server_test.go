42e71eafb97996861effa9307a3eb2c2e01dea0d
rseleznev <gradoozd@gmail.com>
2026-05-23 11:33:30 +0300

    Написал тесты server.storeConn

--- internal/balancer/server_test.go
+++ internal/balancer/server_test.go
@@ -150,3 +150,100 @@ func Test_getConn(t *testing.T) {
 		})
 	}
 }
+
+func Test_storeConn(t *testing.T) {
+	testCases := []struct{
+		name string
+		expectedPoolLen int
+		opts *models.ServerOptions
+		conn mockConn
+		setupFunc func()
+		cleanupFunc func()
+	}{
+		{
+			name: "full path",
+			expectedPoolLen: 1,
+			opts: &models.ServerOptions{
+				MaxSocksPoolLen: 10,
+			},
+			conn: mockConn{
+				checkUnreadFunc: func() (int, error) {
+					return 0, nil
+				},
+				checkUnsentFunc: func() (int, error) {
+					return 0, nil
+				},
+			},
+			setupFunc: func() {
+				testServer.connPool = make(chan models.Conn, 10)
+			},
+			cleanupFunc: func() {
+				testServer.connPool = nil
+			},
+		},
+		{
+			name: "no store - unread/unsent",
+			expectedPoolLen: 0,
+			opts: &models.ServerOptions{
+				MaxSocksPoolLen: 10,
+			},
+			conn: mockConn{
+				checkUnreadFunc: func() (int, error) {
+					return 0, nil
+				},
+				checkUnsentFunc: func() (int, error) {
+					return 15, nil
+				},
+			},
+		},
+		{
+			name: "no store - max pool len",
+			expectedPoolLen: 5,
+			opts: &models.ServerOptions{
+				MaxSocksPoolLen: 5,
+			},
+			conn: mockConn{
+				checkUnreadFunc: func() (int, error) {
+					return 0, nil
+				},
+				checkUnsentFunc: func() (int, error) {
+					return 0, nil
+				},
+			},
+			setupFunc: func() {
+				testServer.connPool = make(chan models.Conn, 5)
+
+				for range 5 {
+					testServer.connPool <- mockConn{}
+				}
+			},
+			cleanupFunc: func() {
+				testServer.connPool = nil
+			},
+		},
+		{
+			name: "no store - no pool",
+			expectedPoolLen: 0,
+			opts: &models.ServerOptions{
+				DisableSocksPool: true,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			testServer.opts = tc.opts
+			if tc.setupFunc != nil {
+				tc.setupFunc()
+			}
+
+			testServer.storeConn(tc.conn)
+			if n := len(testServer.connPool); n != tc.expectedPoolLen {
+				t.Errorf("Ожидаемый размер пула: %d, фактический: %d", tc.expectedPoolLen, n)
+			}
+			if tc.cleanupFunc != nil {
+				tc.cleanupFunc()
+			}
+		})
+	}
+}
