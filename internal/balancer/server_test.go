9981911de2973d15ee66913a0a0ad2c35019e5ff
rseleznev <gradoozd@gmail.com>
2026-05-23 11:13:06 +0300

    Написал тесты server.getConn

--- internal/balancer/server_test.go
+++ internal/balancer/server_test.go
@@ -79,3 +79,74 @@ func Test_init(t *testing.T) {
 		})
 	}
 }
+
+func Test_getConn(t *testing.T) {
+	testCases := []struct{
+		name string
+		expectedErr error
+		opts *models.ServerOptions
+		n mockNetworker // из файла tcp_balancer_test.go
+		setupFunc func()
+	}{
+		{
+			name: "success from pool",
+			expectedErr: nil,
+			opts: &models.ServerOptions{},
+			setupFunc: func() {
+				testServer.connPool <- mockConn{}
+			},
+		},
+		{
+			name: "success pool zero len",
+			expectedErr: nil,
+			opts: &models.ServerOptions{
+				MaxClientsAmount: 10,
+			},
+			n: mockNetworker{
+				newTCPConnFunc: func(_ models.Address) (models.Conn, error) {
+					return mockConn{}, nil
+				},
+			},
+		},
+		{
+			name: "success no pool",
+			expectedErr: nil,
+			opts: &models.ServerOptions{
+				MaxClientsAmount: 10,
+				DisableSocksPool: true,
+			},
+			n: mockNetworker{
+				newTCPConnFunc: func(_ models.Address) (models.Conn, error) {
+					return mockConn{}, nil
+				},
+			},
+		},
+		{
+			name: "fail ErrNoConnsAvailable",
+			expectedErr: models.ErrNoConnsAvailable,
+			opts: &models.ServerOptions{
+				DisableSocksPool: true,
+			},
+			n: mockNetworker{
+				newTCPConnFunc: func(_ models.Address) (models.Conn, error) {
+					return mockConn{}, nil
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			testServer.opts = tc.opts
+			testServer.net = tc.n
+			if tc.setupFunc != nil {
+				tc.setupFunc()
+			}
+
+			_, err := testServer.getConn()
+			if err != tc.expectedErr {
+				t.Errorf("Ожидаемая ошибка: %s, получено: %s", tc.expectedErr, err)
+			}
+		})
+	}
+}
