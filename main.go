d27a9320964b043cef438378210f92d0c8bf6cdd
rseleznev <gradoozd@gmail.com>
2026-06-06 12:34:48 +0300

    Поправил путь к конфигу

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	slog.Info("поллер создан", "module", "main")
 	net := network.NewNet(poller)
 
-	conf := config.Parse("./config/config_sample1.yaml")
+	conf := config.Parse("./config/config_sample.yaml")
 	b := balancer.NewBalancer(conf[0].Balancer, conf[0].Servers, net) // временная передача по индексу
 	if b == nil {
 		slog.Error("ошибка создания балансера", "module", "main", "err", err)
